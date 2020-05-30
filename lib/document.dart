4eb9378a2f2df0bbc6e3a77a645960b0487acb88
Diogo Costa <hello@diogothecoder.com>
2020-05-30 14:59:59 +0100

    Expand Document model to include fromSnapshot and toDocument

--- lib/document.dart
+++ lib/document.dart
@@ -1,3 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:recase/recase.dart';
+
 class Document {
+  final String _name;
+  final List<dynamic> _phrases;
+
   static const WORD_DOCUMENT = 'words';
+
+  Document(this._name, this._phrases);
+
+  static Document fromSnapshot(DocumentSnapshot snapshot) {
+    return Document(
+      snapshot['name'],
+      snapshot['phrases'],
+    );
+  }
+
+  Map<String, Object> toDocument() => {
+        'name': this.getName(),
+        'phrases': this.getPhrases(),
+      };
+
+  String getName() {
+    return this._name;
+  }
+
+  String getTitleCasedName() {
+    return this._name.titleCase;
+  }
+
+  List<dynamic> getPhrases() {
+    return this._phrases;
+  }
 }
