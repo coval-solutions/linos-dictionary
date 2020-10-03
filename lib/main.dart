2dd45b5c76487e1570e29d56685f9f81c40a1902
Diogo Costa <hello@diogothecoder.com>
2020-10-03 20:19:21 +0100

    Firebase, initialise the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -10,11 +10,16 @@ import 'package:linos_dictionary/blocs/simple_bloc_observer.dart';
 import 'package:linos_dictionary/blocs/words_bloc.dart';
 import 'package:linos_dictionary/repos/firestore_words.dart';
 
+import 'config.dart';
+
 Future main() async {
-  await DotEnv().load('.env');
   WidgetsFlutterBinding.ensureInitialized();
 
-  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(kReleaseMode);
+  await DotEnv().load('.env');
+  await Firebase.initializeApp();
+
+  FirebaseCrashlytics.instance
+      .setCrashlyticsCollectionEnabled(Config.isReleaseMode());
 
   Bloc.observer = SimpleBlocObserver();
 
